1c7832e99066cd6683dc358756721ec5265f6585
胡尐睿丶 <304327508@qq.com>
2014-04-04 17:59:36 +0800

    fix bug

--- sysapp/upload/index.ajax.php
+++ sysapp/upload/index.ajax.php
@@ -9,7 +9,7 @@
 	$config = array(
 		'savePath' => 'uploads/member/'.session('member_id').'/file/', //保存路径
 		'allowFiles' => $fileType, //文件允许格式
-		'maxSize' => $uploadFileMaxSize * 1024 //文件大小限制，单位KB
+		'maxSize' => $uploadFileSize * 1024 //文件大小限制，单位KB
 	);
 	$up = new Uploader('file', $config);
 	$info = $up->getFileInfo();
