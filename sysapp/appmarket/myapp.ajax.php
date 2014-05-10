9c040658324e446874fb38c4406436b652a1b8fb
胡尐睿丶 <304327508@qq.com>
2014-05-10 10:51:06 +0800

    修复开发者应用界面应用图标无法选择和上传

--- sysapp/appmarket/myapp.ajax.php
+++ sysapp/appmarket/myapp.ajax.php
@@ -81,9 +81,9 @@
 			$config = array(
 				'savePath' => 'uploads/shortcut/', //保存路径
 				'allowFiles' => array('.jpg', '.jpeg', '.png', '.gif', '.bmp'), //文件允许格式
-				'maxSize' => 1000 //文件大小限制，单位KB
+				'maxSize' => 300 //文件大小限制，单位KB
 			);
-			$up = new Uploader('xfile', $config);
+			$up = new Uploader('file', $config);
 			$info = $up->getFileInfo();
 			echo '{"url":"'.$info['url'].'","fileType":"'.$info['type'].'","original":"'.$info['originalName'].'","state":"'.$info['state'].'"}';
 			break;
