ceeb1350f97d6660b62b4ee3d77f7850600b2436
胡尐睿丶 <304327508@qq.com>
2014-11-24 16:36:28 +0800

    更新登录、注册界面错误提示方式

--- index.php
+++ index.php
@@ -402,8 +402,8 @@ $(function(){
 	var loginForm = $('#loginForm').Validform({
 		btnSubmit: '#submit_login_btn',
 		postonce: false,
-		showAllError: false,
-		tipSweep: true,
+		showAllError: true,
+		tipSweep: false,
 		//msg：提示信息;
 		//o:{obj:*,type:*,curform:*}, obj指向的是当前验证的表单元素（或表单对象），type指示提示的状态，值为1、2、3、4， 1：正在检测/提交数据，2：通过验证，3：验证失败，4：提示ignore状态, curform为当前form对象;
 		//cssctl:内置的提示信息样式控制函数，该函数需传入两个参数：显示提示信息的对象 和 当前提示的状态（既形参o中的type）;
@@ -443,9 +443,9 @@ $(function(){
 	//表单注册初始化
 	var registerForm = $('#registerForm').Validform({
 		btnSubmit: '#submit_register_btn',
-		postonce: true,
-		showAllError: false,
-		tipSweep: true,
+		postonce: false,
+		showAllError: true,
+		tipSweep: false,
 		//msg：提示信息;
 		//o:{obj:*,type:*,curform:*}, obj指向的是当前验证的表单元素（或表单对象），type指示提示的状态，值为1、2、3、4， 1：正在检测/提交数据，2：通过验证，3：验证失败，4：提示ignore状态, curform为当前form对象;
 		//cssctl:内置的提示信息样式控制函数，该函数需传入两个参数：显示提示信息的对象 和 当前提示的状态（既形参o中的type）;
